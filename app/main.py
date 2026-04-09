be784aeff03be265c2a20b8e3777f55c7b646c91
Vitalii <vetal.vodolazkiy@gmail.com>
2026-04-09 22:12:48 +0200

    make list compehention and deleted comments

--- app/main.py
+++ app/main.py
@@ -11,23 +11,19 @@ class Person:
 def create_person_list(people: list) -> list:
     Person.people.clear()
 
-    for person_dict in people:
-        Person(person_dict["name"], person_dict["age"])
+    [Person(person_dict["name"], person_dict["age"]) for person_dict in people]
 
-        # Second pass: link spouses
     for person_dict in people:
         person = Person.people[person_dict["name"]]
 
         wife_name = person_dict.get("wife")
         husband_name = person_dict.get("husband")
 
-        # Link wife if exists
         if wife_name and not getattr(person, "wife", None):
             person.wife = Person.people[wife_name]
             if not getattr(person.wife, "husband", None):
                 person.wife.husband = person
 
-            # Link husband if exists
         if husband_name and not getattr(person, "husband", None):
             person.husband = Person.people[husband_name]
             if not getattr(person.husband, "wife", None):
